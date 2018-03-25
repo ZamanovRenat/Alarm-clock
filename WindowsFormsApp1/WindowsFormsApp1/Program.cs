5d9654f643574e326188c6c61d6e1bc7347af0a2
Renat7797 <zamanov.renat@mail.ru>
2018-03-25 15:39:41 +0300

    Первые наброски

---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // функция PlaySound, обеспечивает воспроизведение wav-файлов, находится в библиотеке winmm.dll подключим эту библиотеку
+        [System.Runtime.InteropServices.DllImport("winmm.dll")]
+        private static extern
+            Boolean PlaySound(string IpszName, int hModule, int dwFlags);
+
+        //Время сигнала (отображения сообщения)
+        private DateTime alarm;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //параметры компонентов
+            numricUpDown1.Maximum = 23;
+            numricUpDown1.Minimum = 0;
+
+            numricUpDown2.Maximum = 59;
+            numricUpDown2.Minimum = 0;
+
+            numricUpDown1.Value = DateTime.Now.Hour;
+            numricUpDown2.Value = DateTime.Now.Minute;
+
+            notifyIcon1.Visible = false;
+
+            //Период обработки сигнала от таймера
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+
+            Label12.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Label12.Text = 
+        }
+
+    }
+}
